962aa8efb602e943dfefd3cbdc90bc34d0d597f4
noman-matteit <noman@matteit.com>
2024-12-10 19:32:14 +0600

    add live url

--- fixoria-server/app.js
+++ fixoria-server/app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://fixoria.matteit.com/"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
