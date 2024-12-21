0dcdb2c00530c32f738f5b4cba2a3fe3c5a25efe
noman-matteit <noman@matteit.com>
2024-12-21 13:18:43 +0600

    remove product type

--- fixoria-server/routes/productRoutes.js
+++ fixoria-server/routes/productRoutes.js
@@ -2,14 +2,11 @@ const router = require("express").Router();
 const upload = require("../middlewares/upload");
 
 const { createProduct } = require("../controllers/product/createProducts");
-const { getProductType } = require("../controllers/product/getProductType");
 const { getAllProducts } = require("../controllers/product/getAllProducts");
 
 // Add `upload.single('image')` middleware for handling image uploads
-router.post("/", upload.single("image"), createProduct);
-
-router.get("/type", getProductType);
-
-router.get("/all", getAllProducts);
+router
+  .get("/all", getAllProducts)
+  .post("/", upload.single("image"), createProduct);
 
 module.exports = router;
